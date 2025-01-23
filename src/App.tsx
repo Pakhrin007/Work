baca4634440a35c368bd1c47abe3dc2f98728038
Aryan <pakhrinp242@gmail.com>
2025-01-23 17:25:17 +0545

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -6,42 +6,47 @@ import CalendarIcon from './assets/images/calendar';
 import SearchIcon from './assets/images/SearchIcon';
 import TimerIcon from './assets/images/TimerIcon';
 import DropDownIcon from './assets/images/chev-down';
+import DocSideBar from './Components/DocumentSidebar/Documentsidebar/DocSideBar';
 function App() {
   return (
     <div className="w-screen h-screen bg-[#10131E] flex">
-      <div className="">
-        <Sidebar />
-      </div>
-      {/* Top horizontal section  */}
-
-      <header className="flex justify-between bg-[#10131E] pl-56 pr-4 h-[48px] w-[1782.73px] p-4 items-center">
-        <div className=" flex gap-10  rounded-md justify-between w-[520px] h-[32px] items-center px-4">
-          <input
-            className="outline-none w-[520px] bg-transparent rounded-md text-white border-white h-[32px]"
-            type="text"
-            placeholder="Search anything"
-          />
-          {/* <SearchIcon  /> */}
-          <div className="h-[14px] w-[14px] text-white cursor-pointer">{<SearchIcon />}</div>
-        </div>
-
-        <div className="flex gap-4 items-center cursor-pointer">
-          <div className="bg-green-600 rounded-full flex justify-around items-center gap-x-3 text-white py-1 px-2">
-            <TimerIcon />
-            <span>05m</span>
-            <DropDownIcon />
-          </div>
-          <h2 className="text-xl text-white">Tue, 24 December, 2024</h2>
-        </div>
-      </header>
-
-      <div className="w-[60px] h-screen bg-[#10131E]">
-        <h3>dvas</h3>
-      </div>
-
-      {/* <Index /> */}
-      {/* <DocSidebarCard /> */}
+      <div>{<DocSideBar />}</div>
     </div>
+    // <div className="w-screen h-screen bg-[#10131E] flex">
+    //   <div className="">
+    //     <Sidebar />
+    //   </div>
+    //   {/* Top horizontal section  */}
+
+    //   <header className="flex justify-between bg-[#10131E] pl-56 pr-4 h-[48px] w-[1782.73px] p-4 items-center">
+    //     <div className=" flex gap-10  rounded-md justify-between w-[520px] h-[32px] items-center px-4">
+    //       <input
+    //         className="outline-none w-[520px] bg-transparent rounded-md text-white border-white h-[32px]"
+    //         type="text"
+    //         placeholder="Search anything"
+    //       />
+    //       {/* <SearchIcon  /> */}
+    //       <div className="h-[14px] w-[14px] text-white cursor-pointer">{<SearchIcon />}</div>
+    //     </div>
+
+    //       <div className="flex gap-4 items-center cursor-pointer">
+    //         <div className='bg-green-600 rounded-full flex justify-around items-center gap-x-3 text-white py-1 px-2'>
+    //         <TimerIcon />
+    //         <span>05m</span>
+    //         <DropDownIcon />
+    //         </div>
+    //         <h2 className='text-xl text-white'>Tue, 24 December, 2024</h2>
+    //       </div>
+    //   </header>
+
+    //   <div className="w-[60px] h-screen bg-[#10131E]">
+    //     <h3>dvas</h3>
+    //   </div>
+
+    //   {/* <Index /> */}
+    //   {/* <DocSidebarCard /> */}
+
+    // </div>
   );
 }
 
--- src/Components/DocumentSidebar/Documentsidebar/DocSideBar.tsx
+++ src/Components/DocumentSidebar/Documentsidebar/DocSideBar.tsx
@@ -1,17 +1,57 @@
 import React from 'react';
 import DocSidebarCard from '../DocumentSidebarCard/DocSidebarCard';
+import Favourites from '../../../assets/images/Favourites';
+import AddIcon from '../../../assets/images/AddIcon';
 const DocSideBar = () => {
   return (
-    <div className="w-[320px] h-[920px] bg-slate-600 rounded-[12px] p-[16px] flex flex-col">
-      <div className="w-[288px] h-[104px] flex flex-col justify-between bg-red-300 mb-[16px]"></div>
-      <div className="w-[288px] h-[116px] flex flex-col justify-between bg-blue-300 mb-[16px]"></div>
+    // ---- main div----
+    <div className="w-[320px] h-[920px] bg-dark rounded-[12px] p-[16px] flex flex-col">
+      {/* ---first div---- */}
+      <div className="w-[288px] h-[104px] flex flex-col justify-between  mb-[16px] sace-y-[12px]">
+        <div className="w-[288px] h-[56px]  sapce-y-[4px]">
+          <p className="text-white text-xl font-medium  leading-loose">App Projects</p>
+          <p className="text-white/80 text-sm font-normal leading-tight">
+            You have 42 new projects
+          </p>
+        </div>
+        <button className="w-[288px] h-[36px] space-y-[4px] px-[4px] rounded-[8px]  bg-white/5 flex justify-center items-center">
+          <div>{<AddIcon />}</div>
+          <div className="text-white/80 text-xs font-medium font-['Manrope'] leading-none">
+            Add New
+          </div>
+        </button>
+      </div>
+      {/* ---second div---- */}
+      <div className="w-[288px] max-h-[300px] flex flex-col mb-[16px] space-y-[8px]">
+        <div className="flex items-center h-[16px] gap-x-[8px] ">
+          <i>{<Favourites />}</i>
+          <p className="  text-white text-xs font-medium font-['Manrope'] leading-none">
+            Favourites
+          </p>
+        </div>
 
-      <div className="w-[288px] h-[260px] flex flex-col  bg-green-300 gap-y-[12px]">
-        <div className="text-white h-[16px]">App Projects</div>
-        <div>{<DocSidebarCard />}</div>
-        <div>{<DocSidebarCard />}</div>
-        <div>{<DocSidebarCard />}</div>
-        <div>{<DocSidebarCard />}</div>
+        {/* ---third div---- */}
+        <div className=" flex flex-col overflow-y-auto overflow-x-hidden max-h-[200px] space-y-[8px] ">
+          <DocSidebarCard />
+          <DocSidebarCard />
+          <DocSidebarCard />
+          <DocSidebarCard />
+        </div>
+      </div>
+
+      {/* Divider */}
+      <div className="w-[288px] h-[1px] bg-white/10 mb-[16px]" />
+
+      <div className="w-[288px] h-[260px] flex flex-col gap-y-[12px]">
+        <div className="text-white text-xs font-medium font-['Manrope'] leading-none">
+          App Projects
+        </div>
+        <div className="space-y-[8px] overflow-y-auto overflow-x-hidden max-h-[200px]">
+          <div>{<DocSidebarCard />}</div>
+          <div>{<DocSidebarCard />}</div>
+          <div>{<DocSidebarCard />}</div>
+          <div>{<DocSidebarCard />}</div>
+        </div>
       </div>
     </div>
   );
